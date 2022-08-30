0cf5981c7d6721233d1d3ca5f8d3d9d44daf3b31
Tomas Tornamira <103047119+TTorna@users.noreply.github.com>
2022-08-30 08:12:39 -0300

    Add files via upload

---
+++ Hilos/TP 2 Hilos/TP 2 Hilos/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(12, 188)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(776, 250)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(118, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(188, 51)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Cancelar"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(96, 46)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(564, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(118, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(308, 32)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Iniciar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(666, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 24)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Porcentaje"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.Color.Black
+        Me.TextBox1.Location = New System.Drawing.Point(539, 118)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 9
+        Me.TextBox1.Text = "Cant. de lineas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Hilos/TP 2 Hilos/TP 2 Hilos/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.Threading
+Imports System.IO.StreamWriter
+Imports System.IO
+Imports System.Text
+Imports System.dll
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    <STAThread()>
+    Public Shared Sub Main()
+        'crear 2 hilos diferentes, cada hilo se enlaza con un método para iniciar los formularios
+        Dim hilo1 As New Thread(AddressOf Proceso1)
+        Dim hilo2 As New Thread(AddressOf Proceso2)
+        'después se arrancan los 2 hilos
+        hilo1.Start()
+        hilo2.Start()
+    End Sub
+
+    Public Shared Sub Proceso1()
+        Dim Texto As String = Form1.TextBox2.Text
+        Proceso2(Texto)
+    End Sub
+    'procedimiento que muestra el segundo formulario
+    Public Shared Sub Proceso2(Aux As String)
+
+
+
+        'Form1.TextBox2.Text
+
+        Dim Palabras As String() = Form1.TextBox2.Text.Split(" ")
+        Dim Palabra As String = ""
+        Dim Contador As Integer = 0
+
+        Dim Linea As String = ""
+        Contador = 0
+        For Each Palabra In Palabras
+            Linea = Linea & Palabra & " "
+            Contador += 1
+            'Contador += Palabra.Length
+
+        Next
+        'MsgBox(Palabra)
+
+        Dim lineas As String() = Form1.TextBox2.Lines()
+        Contador -= 1
+        For Each lin As String In lineas
+            Dim values As Object() = {lin}
+            'MsgBox(lin)
+            Contador += 1
+        Next
+
+        'Form1.ProgressBar1.Value = Contador
+        'Form1.Label2.Text = (Contador / Max) * 100 & "%"
+        'Form1.Label2.Refresh()
+        'Threading.Thread.Sleep(100)
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Proceso1()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Val(TextBox1.Text) > 0 Then
+            Button2.Visible = False
+            Button1.Visible = True
+            TextBox2.Enabled = True
+            TextBox1.Enabled = False
+        Else
+            MsgBox("Inserte cantidad maxima de lineas de texto")
+        End If
+
+        BackgroundWorker1.RunWorkerAsync()
+
+        Dim Max As Integer = Val(TextBox1.Text)
+        ProgressBar1.Maximum = Max
+
+        Dim path As String = "C:\Users\PC1\Desktop\Hilos.txt"
+
+        ' Create or overwrite the file.
+        Dim fs As FileStream = File.Create(path)
+
+        ' Add text to the file.
+        Dim info As Byte()
+
+        For a = 1 To Max
+            info = New UTF8Encoding(True).GetBytes("Linea " + Str(a) + vbCrLf)
+            fs.Write(info, 0, info.Length)
+        Next
+
+        fs.Close()
+
+    End Sub
+
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+
+        Dim i As Integer
+
+        For i = 1 To 1000
+            BackgroundWorker1.WorkerReportsProgress = True
+            BackgroundWorker1.ReportProgress(i / 10)
+
+            Threading.Thread.Sleep(25)
+
+        Next
+
+    End Sub
+    Private Sub BackgroundWorker1_ProgressChanged(ByVal sender As Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+
+        ProgressBar1.Value = e.ProgressPercentage
+
+    End Sub
+
+
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+
+        MsgBox("Terminado")
+        me.hide()
+
+    End Sub
+
+End Class
